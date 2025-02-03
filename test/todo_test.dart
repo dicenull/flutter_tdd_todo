2d22bf76a7857219497882f1cb1f859336c14acc
dicenull <dicenull.dev@gmail.com>
2025-02-03 17:51:05 +0900

    fail add todo test

--- test/todo_test.dart
+++ test/todo_test.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:todos/main.dart';
+import 'package:todos/todo.dart';
 import 'package:todos/todo_data.dart';
 
 void main() {
@@ -7,5 +10,20 @@ void main() {
     final todo2 = Todo(id: '1', description: 'shopping');
 
     expect(todo1 == todo2, isTrue);
+  });
+
+  group(TodoList, () {
+    test('Todoを追加できる', () {
+      // ProviderScopeの代わりにProviderContainerを作成
+      final container = ProviderContainer();
+      // TodoListNotifierがdisposeされないように
+      final subscription = container.listen(todoListProvider, (_, __) {});
+      // TodoListNotifierを取得
+      final todoList = container.read(todoListProvider.notifier);
+
+      todoList.add('buy');
+
+      expect(subscription.read().last, Todo(description: 'buy', id: '1'));
+    });
   });
 }
